f54a50e36e7d5255b3d1be1c35a5ce04ce052bfe
Rocio <rfernm09@users.noreply.github.com>
2017-12-27 21:52:03 +0100

    Update powerlifting.c

--- powerlifting.c
+++ powerlifting.c
@@ -24,12 +24,16 @@ int main (int argc, char *argv[]) {
 	for (int i=0;i<10;i++) {
 		printf("Atleta %d: ha competido %d, su tarima actual es %d y necesita beber %d\n",atletas[i].id,atletas[i].ha_competido,atletas[i].tarima_asignada,atletas[i].necesita_beber);
 	}
-	printf("Hueco para nuevo atleta: %d\n",haySitioEnCampeonato());
+	if (haySitioEnCampeonato()==-1) {
+		printf("La inscripción al campeonato está completa. No se puede inscribir aún.\n");
+	} else {
+		printf("Hueco para nuevo atleta: %d\n",haySitioEnCampeonato());
+	}
 }
 
 void inicializaCampeonato() {
 	for (int i=0;i<10;i++) {
-		atletas[i].id=i;
+		atletas[i].id=0;
 		atletas[i].ha_competido=0;
 		atletas[i].tarima_asignada=0;
 		atletas[i].necesita_beber=0;
