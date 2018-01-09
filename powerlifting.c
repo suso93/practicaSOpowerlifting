ab5190f2e12375aacfcb6642066305c3d0c80d5e
Rocio <rfernm09@users.noreply.github.com>
2018-01-09 12:26:24 +0100

    Update powerlifting.c

--- powerlifting.c
+++ powerlifting.c
@@ -11,18 +11,29 @@
 
 //contador de atletas que participen a lo largo del campeonato
 int cuantos_atletas=0;
+/*int atletas_tarima1;
+int atletas_tarima2;*/
 
-//inicializar los semaforos
+//inicializar los semaforos:
 pthread_mutex_t mutex,semaforo_atletas;
 
 struct atletas_competicion {
 	int id;
 	int ha_competido;
 	int tarima_asignada;
-	int puntuacion;//
+	int puntuacion;
 	int necesita_beber;
 };
 struct atletas_competicion atletas[maximoAtletas];
+
+
+/*//?Creamos struct para las tarimas? Sería algo asi:
+struct tarimas_competicion {
+	int id;
+	int estado;//0 para libre, 1 para ocupada, 2 para cerrada
+	int atletas;//contador de atletas que han pasado por esta tarima
+};
+struct tarimas_competicion tarima[numTarimas];*/
 
 FILE *ficherolog;
 
@@ -36,8 +47,8 @@ int haySitioEnCampeonato();//nos dirá si hay sitio (y si lo hay nos dice el pri
 void nuevoCompetidor();//en realidad esta funcion creo que es la que usaremos para cuando se recibe la señal
 //void competidorATarima1();
 void finalizaCompeticion();
-void accionesAtleta();//le pasaremos el atleta (VER ¿id?/¿posicion?)
-void accionesTarima();
+void accionesAtleta();//le pasaremos el atleta (VER ¿id?/¿posicion?) y la tarima
+void accionesTarima();//le pasaremos la tarima
 void  writeLogMessage(char *id, char *msg);
 
 
@@ -105,7 +116,7 @@ int haySitioEnCampeonato() {
 	return -1;
 }//devuelve -1 en caso de no haber, y sino nos da el primer hueco que encuentre
 
-void nuevoCompetidor (){ //¿ponemos que reciba la tarima cuando implementemos más de una?
+void nuevoCompetidor (){ 
 //al recibir SIGUSR1:
 	int posicion;
 	if(signal(SIGUSR1,nuevoCompetidor)==SIG_ERR) {
@@ -119,17 +130,17 @@ void nuevoCompetidor (){ //¿ponemos que reciba la tarima cuando implementemos m
 		posicion=haySitioEnCampeonato();
 		atletas[posicion].id=cuantos_atletas;
 		atletas[posicion].puntuacion=0;
-		atletas[posicion].tarima_asignada=1;//al principio solo una tarima, ademas con SIGUSR1 va para tarima1
+		atletas[posicion].tarima_asignada=1;//al principio solo una tarima, ademas con SIGUSR1 va para tarima1 //DUDA: ¿aqui se ponia la tarima o se indicaba si la tenia asignada o no?
 		atletas[posicion].ha_competido=0;
 		atletas[posicion].necesita_beber=0;
 		//creo hilo de atleta:
 		//pthread_create();
-		
+		AccionesAtleta();//ver qué le pasamos, creo que habria que pasarle id atleta y id tarima
 	} else { 
 		printf("Ya están inscritos y participando 10 atletas, de momento no puedes participar\n");
 	}
 }
-void AccionesAtleta (){
+void AccionesAtleta (){ //
 	//guardar en log:
 	//hora de entrada a tarima y a cual
 	//calculo del comportamiento del atleta
@@ -144,7 +155,9 @@ void AccionesTarima (){
 }
 void finalizaCompeticion (){
 	printf("Has pulsado finalizar competicion\n");
-	//se realiza todo lo necesario para terminar con los atletas activos
+	//Parar de recibir señales!!!!
+
+	//Y se realiza todo lo necesario para terminar con los atletas activos
 	//PRUEBA para ir viendo como están en el momento de la petición:
 	for (int i=0;i<maximoAtletas;i++) {
 		printf("Atleta %d: ha competido %d, su tarima actual es %d y necesita beber %d\n",atletas[i].id,atletas[i].ha_competido,atletas[i].tarima_asignada,atletas[i].necesita_beber);
@@ -153,363 +166,6 @@ void finalizaCompeticion (){
 	printf("Te mostraré los resultados\n");//
 	signal(SIGTERM, SIG_DFL);
 	raise(SIGTERM);
-}
-void  writeLogMessage(char *id, char *msg) {
-	//la hora  actual
-	time_t  now = time (0);
-	struct  tm *tlocal = localtime (&now);
-	char  stnow [19];
-	strftime(stnow , 19, " %d/ %m/ %y  %H: %M: %S", tlocal);
-	//  Escribimos  en el log
-	ficherolog = fopen("ficherolog.log" , "a");
-	fprintf(ficherolog , "[ %s]  %s:  %s\n", stnow , id, msg);
-	fclose(ficherolog);
-}
-				exit(-1);
-	}
-
-	//inicializar recursos:
-	/*if (pthread_mutex_init(&semaforo_atletas, NULL)!=0)
-+	{
-+		perror("Error en la creación del semáforo de los atletas.\n");
-+		exit(-1);
- 	}*/
-	//el contador de atletas lo tenemos inicializado arriba
-	inicializaCampeonato();
-	//tarimas:crear los 2 hilos de tarimas (primero solo 1)
-	//pthread_create(...);
-	FILE *ficherolog = fopen ("ficherolog.log","w"); //errores al abrir?
-
-	//visualizo la estructura inicial PARA IR PROBANDO -> BORRAR: 
-	for (int i=0;i<10;i++) {
-		printf("Atleta %d: ha competido %d, su tarima actual es %d y necesita beber %d\n",atletas[i].id,atletas[i].ha_competido,atletas[i].tarima_asignada,atletas[i].necesita_beber);
-	}
-	printf("El pid del campeonato es %d\n",getpid());
-	if (haySitioEnCampeonato()==-1) {
-		printf("La inscripción al campeonato está completa. No se puede inscribir aún.\n");
-	} else {
-		printf("Hueco para nuevo atleta: %d\n",haySitioEnCampeonato());
-	}//HASTA AKI BORRAR
-
-	//espera por señales:
-	while(1) {
-		
-	}
-	
-}
-
-void inicializaCampeonato() {
-	for (int i=0;i<10;i++) {
-		atletas[i].id=0;
-		atletas[i].ha_competido=0;
-		atletas[i].tarima_asignada=0;
-		atletas[i].puntuacion=0;
-		atletas[i].necesita_beber=0;
-	}
-} //es posible que no lo necesitemos ya que se inicializan al crearse un nuevo competidor.. 
-
-int haySitioEnCampeonato() {
-	for (int i=0;i<10;i++) {
-		if (atletas[i].id==0) {
-			return i;
-		}
-	}
-	return -1;
-}//devuelve -1 en caso de no haber, y sino nos da el primer hueco que encuentre
-
-void nuevoCompetidor (){
-//al recibir SIGUSR1:
-	int posicion;
-	if(signal(SIGUSR1,nuevoCompetidor)==SIG_ERR) {
-				perror("Llamada a signal.");
-				exit(-1);
-	}
-	printf("Un atleta ha solicitado inscribirse...\n");
-	if(haySitioEnCampeonato()!=-1) {
-		printf("Vas a ser inscrito\n");
-		cuantos_atletas++;
-		posicion=haySitioEnCampeonato();
-		atletas[posicion].id=cuantos_atletas;
-		atletas[posicion].puntuacion=0;
-		atletas[posicion].tarima_asignada=1;//al principio solo una tarima, ademas con SIGUSR1 va para tarima1
-		atletas[posicion].ha_competido=0;
-		atletas[posicion].necesita_beber=0;
-		//creo hilo de atleta:
-		//pthread_create();
-		
-	} else { 
-		printf("Ya están inscritos y participando 10 atletas, de momento no puedes participar\n");
-	}
-}
-void AccionesAtleta (){
-	//guardar en log:
-	//hora de entrada a tarima y a cual
-	//calculo del comportamiento del atleta
-		
-}
-
-void AccionesTarima (){
-	//busca primer atleta en espera de su cola, sino el primero de la otra tarima
-
-
-
-}
-void finalizaCompeticion (){
-	printf("Has pulsado finalizar competicion\n");
-	//se realiza todo lo necesario para terminar con los atletas activos
-	sleep(3);
-	printf("Te mostraré los resultados\n");
-	signal(SIGTERM, SIG_DFL);
-	raise(SIGTERM);
-}
-void  writeLogMessage(char *id, char *msg) {
-	//la hora  actual
-	time_t  now = time (0);
-	struct  tm *tlocal = localtime (&now);
-	char  stnow [19];
-	strftime(stnow , 19, " %d/ %m/ %y  %H: %M: %S", tlocal);
-	//  Escribimos  en el log
-	ficherolog = fopen("ficherolog.log" , "a");
-	fprintf(ficherolog , "[ %s]  %s:  %s\n", stnow , id, msg);
-	fclose(ficherolog);
-}
-				exit(-1);
-	}
-	//inicializar recursos
-	/*if (pthread_mutex_init(&semaforo_atletas, NULL)!=0)
-+	{
-+		perror("Error en la creación del semáforo de los atletas.\n");
-+		exit(-1);
- 	}*/
-	inicializaCampeonato();
-	
-	FILE *ficherolog = fopen ("ficherolog.log","w"); //errores al abrir?
-	//visualizo la estructura inicial: 
-	for (int i=0;i<10;i++) {
-		printf("Atleta %d: ha competido %d, su tarima actual es %d y necesita beber %d\n",atletas[i].id,atletas[i].ha_competido,atletas[i].tarima_asignada,atletas[i].necesita_beber);
-	}
-	printf("El pid del campeonato es %d\n",getpid());
-	if (haySitioEnCampeonato()==-1) {
-		printf("La inscripción al campeonato está completa. No se puede inscribir aún.\n");
-	} else {
-		printf("Hueco para nuevo atleta: %d\n",haySitioEnCampeonato());
-	}
-	while(1) {
-		
-	}
-	
-}
-
-void inicializaCampeonato() {
-	for (int i=0;i<10;i++) {
-		atletas[i].id=0;
-		atletas[i].ha_competido=0;
-		atletas[i].tarima_asignada=0;
-		atletas[i].puntuacion=0;
-		atletas[i].necesita_beber=0;
-	}
-}
-
-int haySitioEnCampeonato() {
-	for (int i=0;i<10;i++) {
-		if (atletas[i].id==0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-void nuevoCompetidor (){
-//al recibir SIGUSR1:
-	int posicion;
-	if(signal(SIGUSR1,nuevoCompetidor)==SIG_ERR) {
-				perror("Llamada a signal.");
-				exit(-1);
-	}
-	printf("Un atleta ha solicitado inscribirse...\n");
-	if(haySitioEnCampeonato()!=-1) {
-		printf("Vas a ser inscrito\n");
-		cuantos_atletas++;
-		posicion=haySitioEnCampeonato();
-		atletas[posicion].id=cuantos_atletas;
-	} else {
-		printf("Ya están inscritos y participando 10 atletas, de momento no puedes participar\n");
-	}
-}
-void AccionesAtleta (){
-
-}
-/*void competidorATarima1 (){
-	//al recibir SIGUSR1 meter crear atleta y meter en la cola de la tarima1
-}*/
-void AccionesTarima (){
-
-}
-void finalizaCompeticion (){
-	printf("Has pulsado finalizar competicion\n");
-	//se realiza todo lo necesario para terminar con los atletas activos
-	sleep(3);
-	printf("Te mostraré los resultados\n");
-	signal(SIGTERM, SIG_DFL);
-	raise(SIGTERM);
-}
-void  writeLogMessage(char *id, char *msg) {
-	//la hora  actual
-	time_t  now = time (0);
-	struct  tm *tlocal = localtime (&now);
-	char  stnow [19];
-	strftime(stnow , 19, " %d/ %m/ %y  %H: %M: %S", tlocal);
-	//  Escribimos  en el log
-	ficherolog = fopen("ficherolog.log" , "a");
-	fprintf(ficherolog , "[ %s]  %s:  %s\n", stnow , id, msg);
-	fclose(ficherolog);
-}
-				exit(-1);
-	}
-	//inicializar recursos
-	/*if (pthread_mutex_init(&semaforo_atletas, NULL)!=0)
-+	{
-+		perror("Error en la creación del semáforo de los atletas.\n");
-+		exit(-1);
- 	}*/
-	inicializaCampeonato();
-	
-	FILE *ficherolog = fopen ("ficherolog.log","w"); //errores al abrir?
-	//visualizo la estructura inicial: 
-	for (int i=0;i<10;i++) {
-		printf("Atleta %d: ha competido %d, su tarima actual es %d y necesita beber %d\n",atletas[i].id,atletas[i].ha_competido,atletas[i].tarima_asignada,atletas[i].necesita_beber);
-	}
-	printf("El pid del campeonato es %d\n",getpid());
-	if (haySitioEnCampeonato()==-1) {
-		printf("La inscripción al campeonato está completa. No se puede inscribir aún.\n");
-	} else {
-		printf("Hueco para nuevo atleta: %d\n",haySitioEnCampeonato());
-	}
-	while(1) {
-		
-	}
-	
-}
-
-void inicializaCampeonato() {
-	for (int i=0;i<10;i++) {
-		atletas[i].id=0;
-		atletas[i].ha_competido=0;
-		atletas[i].tarima_asignada=0;
-		atletas[i].puntuacion=0;
-		atletas[i].necesita_beber=0;
-	}
-}
-
-int haySitioEnCampeonato() {
-	for (int i=0;i<10;i++) {
-		if (atletas[i].id==0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-void nuevoCompetidor (){
-//al recibir SIGUSR1:
-	int posicion;
-	if(signal(SIGUSR1,nuevoCompetidor)==SIG_ERR) {
-				perror("Llamada a signal.");
-				exit(-1);
-	}
-	printf("Un atleta ha solicitado inscribirse...\n");
-	if(haySitioEnCampeonato()!=-1) {
-		printf("Vas a ser inscrito\n");
-		cuantos_atletas++;
-		posicion=haySitioEnCampeonato();
-		atletas[posicion].id=cuantos_atletas;
-	} else {
-		printf("Ya están inscritos y participando 10 atletas, de momento no puedes participar\n");
-	}
-}
-void AccionesAtleta (){
-
-}
-/*void competidorATarima1 (){
-	//al recibir SIGUSR1 meter crear atleta y meter en la cola de la tarima1
-}*/
-void AccionesTarima (){
-
-}
-void finalizaCompeticion (){
-	printf("Has pulsado finalizar competicion\n");
-	//se realiza todo lo necesario para terminar con los atletas activos
-	sleep(3);
-	printf("Te mostraré los resultados\n");
-	signal(SIGTERM, SIG_DFL);
-	raise(SIGTERM);
-}
-void  writeLogMessage(char *id, char *msg) {
-	//la hora  actual
-	time_t  now = time (0);
-	struct  tm *tlocal = localtime (&now);
-	char  stnow [19];
-	strftime(stnow , 19, " %d/ %m/ %y  %H: %M: %S", tlocal);
-	//  Escribimos  en el log
-	ficherolog = fopen("ficherolog.log" , "a");
-	fprintf(ficherolog , "[ %s]  %s:  %s\n", stnow , id, msg);
-	fclose(ficherolog);
-}
-				exit(-1);
-	}
-	//inicializar recursos
-	if (pthread_mutex_init(&semaforo_atletas, NULL)!=0)
-+	{
-+		perror("Error en la creación del semáforo de los atletas.\n");
-+		exit(-1);
- 	}
-	inicializaCampeonato();
-	
-	FILE *ficherolog = fopen ("ficherolog.log","w"); //errores al abrir?
-	//visualizo la estructura inicial: 
-	for (int i=0;i<10;i++) {
-		printf("Atleta %d: ha competido %d, su tarima actual es %d y necesita beber %d\n",atletas[i].id,atletas[i].ha_competido,atletas[i].tarima_asignada,atletas[i].necesita_beber);
-	}
-	if (haySitioEnCampeonato()==-1) {
-		printf("La inscripción al campeonato está completa. No se puede inscribir aún.\n");
-	} else {
-		printf("Hueco para nuevo atleta: %d\n",haySitioEnCampeonato());
-	}
-	
-}
-
-void inicializaCampeonato() {
-	for (int i=0;i<10;i++) {
-		atletas[i].id=0;
-		atletas[i].ha_competido=0;
-		atletas[i].tarima_asignada=0;
-		atletas[i].puntuacion=0;
-		atletas[i].necesita_beber=0;
-	}
-}
-
-int haySitioEnCampeonato() {
-	for (int i=0;i<10;i++) {
-		if (atletas[i].id==0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-void nuevoCompetidor (){
-
-}
-void AccionesAtleta (){
-
-}
-void competidorATarima1 (){
-	//al recibir SIGUSR1 meter crear atleta y meter en la cola de la tarima1
-}
-void AccionesTarima (){
-
-}
-void finalizaCompeticion (){
 }
 void  writeLogMessage(char *id, char *msg) {
 	//la hora  actual
